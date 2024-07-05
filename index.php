d91532bd63a1ecfb640d2f9a2f888ffd761214e4
juane <juanantonioespinozasevilla@gmail.com>
2024-07-04 23:41:18 -0600

    prueba-2

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php require_once "./parte_superior.php" ?>
+
+<div class="titulo">
+    <p>
+        Estudiar una carrera universitaria es un privilegio de pocos, ¿qué estás esperando?
+    </p>
+</div>
+
+<body>
+    <div class="main-img">
+        <a href="./index.php"> <img src="./imagenes/main-img.jpeg" alt=""></a>
+    </div>
+
+    <div class="main-accordion-container">
+
+    </div>
+
+
+
+    <div class="mid-info">
+        <p class="mid-info-title">Oferta Academica</p>
+        <p class="mid-info-desc">Decidete y escoge, que quieres estudiar</p>
+    </div>
+
+    <div class="card-list">
+
+    </div>
+
+
+
+
+</body>
+
+
+
+<?php require_once "./parte_inferior.php" ?>
