e26cc73d3d074a9bae8df0a18a4824b53fc96160
juane <juanantonioespinozasevilla@gmail.com>
2024-07-05 00:57:38 -0600

    primera-prueba

---
+++ parte_inferior.php
@@ -0,0 +1,17 @@
+<footer>
+    <div class="footer-izquierda">
+        <h2>Tercera I, II cuatrimestre, 2024</h2>
+    </div>
+    <div class="footer-derecha">
+        <p>Todos los derechos reservados</p>
+        <p>Esta es una pagina de prueba</p>
+        <p>2024-2023</p>
+    </div>
+</footer>
+
+
+
+
+
+<script src="./script/hamburger-icon.js"></script>
+<script src="./script/accordion.js"></script>
---
+++ parte_superior.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="proyecto III gen I" content="Pagina de prueba">
+    <title>
+        Samsung
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+
+<menu>
+
+    <div class="barra">
+
+        <a href="https:google.com">Tienda Online</a>
+        <a href="https:google.com">AI</a>
+        <a href="https:google.com">Mobile</a>
+        <a href="https:google.com">TV y AV</a>
+        <a href="https:google.com">Electrodomesticos</a>
+        <a href="https:google.com">Informatica</a>
+        <a href="https:google.com">Accesorios</a>
+        <a href="https:google.com">Smart Things</a>
+        <a href="https:google.com">Soporte</a>
+
+    </div>
+
+    <div class="menu-izquierda">
+        <a href="./index.php"> <img src="./imagenes/logo1.jpeg" alt=""></a>
+    </div>
+
+    <div class="menu-container">
+        <div class="menu-derecha">
+            <a class="enlaces-menu-derecha" href="https:google.com">primer ingreso</a>
+            <a class="enlaces-menu-derecha" href="https:google.com">Aula En Linea</a>
+        </div>
+
+        <div class="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+
+        </div>
+
+    </div>
+
+
+</menu>
