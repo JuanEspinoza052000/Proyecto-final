0e515e2c5e4a06ca866537295285d8b1b8f7380e
juane <juanantonioespinozasevilla@gmail.com>
2024-07-07 19:42:12 -0600

    segunda-prueba

---
+++ parte_superior.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="proyecto III gen I" content="Pagina de prueba">
+    <title>
+        Tercera I
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php"> <img src="./imagenes/logo1.png" alt=""></a>
+    </div>
+
+    <div class="menu-container">
+
+
+
+    <div class="barra-nav">
+        <p>Tienda online</p>
+        <p>AI</p>
+        <p>Mobile</p>
+        <p>Tv y AV</p>
+        <p>Electrodomesticos</p>
+        <p>Informatica</p>
+        <p>Accesorios</p>
+
+
+
+
+
+    </div>
+
+
+
+        <div class="menu-derecha">
+            <a class="enlaces-menu-derecha" href="https:google.com">primer ingreso</a>
+            <a class="enlaces-menu-derecha" href="https:google.com">Aula En Linea</a>
+        </div>
+
+        <div class="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+
+        </div>
+
+    </div>
+
+
+</menu>
